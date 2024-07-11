466b997f03730cd6d8f5ca500fce6ea52335d672
Jose Luis Elisseche <joselise61@gmail.com>
2024-07-11 18:43:42 -0300

    Modificaciones varias

---
+++ Contenedor/App1/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+# Create your views here.
+
+# def IndexView(request):
+#    ''' Esto es la pagina principal'''
+#    return HttpResponse("Hola Mundo")'''
+
+def IndexView(request):
+    ''' Esto es la pagina principal'''
+    return render(request,"index.html")
+
+
